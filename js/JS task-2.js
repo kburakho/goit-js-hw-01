648c21d2457db6dafe9236a66ba37c6512c7d41c
Konstantin <timofeykovalskiy60@gmail,com>
2024-10-31 15:44:40 +0200

    hm1-add_scripts

--- js/JS task-2.js
+++ js/JS task-2.js
@@ -6,5 +6,3 @@ function getShippingMessage(country, price, deliveryFee) {
 console.log(getShippingMessage('Australia', 120, 50));
 console.log(getShippingMessage('Germany', 80, 20));
 console.log(getShippingMessage('Sweden', 100, 20));
-
-console.log(getShippingMessage(`Ukraine`, 1500, 500));
